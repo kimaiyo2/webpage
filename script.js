f1ddd468a456f503b38582092e232074928f1c2a
isaac kimaiyo <isaackimaiyo704@gmail.com>
2025-05-06 12:35:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//
+function addNumbers(a,b) {
+    let c= a+b;
+    return c;
+}
+function multiply(x,y) {
+    let w= x*y;
+    return w;
+}
+function greet(){
+    console.log("Hello isaac kimaiyo");
+}
+console.log(addNumbers(20, 2));
+console.log(multiply(20, 2));
+greet();
